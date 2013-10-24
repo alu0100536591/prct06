b513376dfd43ce4f1321c5300555624e0e266646
Jorge O. Blanchard Cruz <rothgard@rothgard-VirtualBox.(none)>
2013-10-24 20:36:47 +0000

    4 Commit: Implementando Fraction - Hasta comparacion (==)

--- lib/Fraction.rb
+++ lib/Fraction.rb
@@ -1,10 +1,34 @@
+
+require "gcd"
+
 class Fraction
 
   	attr_accessor :_num, :_den
 	
 	def initialize(n,d)
 		@_num, @_den = n, d
-		#minimize()
+	end
+	
+	def minimize #minimiza dividiendo por el mínimo común divisor
+		gcd = gcd(@_num, @_den)
+		@_num /= gcd
+		@_den /= gcd
+	end
+	
+	def to_s
+		"#{@_num}/#{@_den}"
+	end
+	
+	def to_float
+		"#{@_num/@_den}"
+	end
+	
+	def ==(frac) #Compara dos fracciones
+		if (@_num = frac._num) and (@_den = frac._den)
+			true
+		else
+			false
+		end
 	end
   
 end
--- test/Fraction_spec.rb
+++ test/Fraction_spec.rb
@@ -1,24 +1,68 @@
 require 'Fraction'
 
 describe Fraction do
-  before :each do
-	@numerador = 3
-	@denominador = 9
-    @FractionA = Fraction.new(@numerador, @denominador)
-  end
-  
-  describe "# almacenamiento de variables" do
-  
-    it "Debe existir un numerador" do
-		@FractionA._num.should eq(@numerador)
+	before :each do
+		@numerador = 3
+		@denominador = 9
+		@FractionA = Fraction.new(@numerador, @denominador)
+		@FractionB = Fraction.new(@numerador, @denominador)
 	end
-	  
-	it "Debe existir un denominador" do
-		@FractionA._den.should eq(@denominador)
-	end  
-
-  end
   
+	describe "# almacenamiento de variables" do
+		it "Debe existir un numerador" do
+			@FractionA._num.should eq(@numerador)
+		end
+	  
+		it "Debe existir un denominador" do
+			@FractionA._den.should eq(@denominador)
+		end  
+	end
+  
+  
+	describe "# prueba de métodos" do
+		it "Debe de estar en su forma reducida" do
+			@FractionA.minimize()
+			@FractionA._num.should eq(1)
+			@FractionA._den.should eq(3)
+		end
+		
+		it "Se debe invocar al metodo _num para obtener el numerador" do
+			@FractionA._num.should eq(@numerador)
+		end
+		
+		it "Se debe invocar al metodo _den para obtener el denominador" do
+			@FractionA._den.should eq(@denominador)
+		end
+		
+		it "Se debe mostar por la consola la fraccion de la forma: a/b, donde a es el numerador y b el denominador" do
+			@FractionA.to_s.should eq("3/9")
+		end
+		
+		 it "Se debe mostar por la consola la fraccion en formato flotante" do
+			@FractionA._num = 3.0
+			@FractionA._den = 9.0
+			@FractionA.to_float.should eq("0.333333333333333")
+		 end
+		 
+		it "Se debe comparar si dos fracciones son iguales con ==" do
+			@FractionA.==(Fraction.new(@numerador, @denominador)).should eq(true)
+		end
+		
+  #Se debe calcular el valor absoluto de una fraccion con el metodo abs
+  #Se debe calcular el reciproco de una fraccion con el metodo reciprocal
+  #Se debe calcular el opuesto de una fraccion con -
+  #Se debe sumar dos fracciones con + y dar el resultado de forma reducida
+  #Se debe restar dos fracciones con - y dar el resultado de forma reducida
+  #Se debe multiplicar dos fracciones con * y dar el resultado de forma reducida
+  #Se debe dividir dos fracciones con / y dar el resultado de forma reducida
+  #Se debe calcular el resto dos fracciones con % y dar el resultado de forma reducida
+  #Se debe de poder comprobar si una fracion es menor que otra
+  #Se debe de poder comprobar si una fracion es mayor que otra
+  #Se debe de poder comprobar si una fracion es menor o igual que otra
+  #Se debe de poder comprobar si una fracion es mayor o igual que otra
+	end
+	
+
 
   
 end
